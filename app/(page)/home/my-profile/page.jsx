95048efca8ad9af5466b178b9e53e13d99f47c60
Keyur Dobariya <keyur.teqheal@gmail.com>
2025-07-11 12:45:49 +0530

    Update page.jsx

--- app/(page)/home/my-profile/page.jsx
+++ app/(page)/home/my-profile/page.jsx
@@ -1,7 +1,8 @@
+// Ensure this component is executed only on the client-side
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import CardProfilePage from '../CardProfilePage';
 
 export default function Page() {
@@ -9,13 +10,20 @@ export default function Page() {
     const [employeeCode, setEmployeeCode] = useState(null);
 
     useEffect(() => {
+        // Get the `user` parameter from searchParams on the client-side
         const user = searchParams.get('user');
         setEmployeeCode(user);
-    }, [searchParams]);
+    }, [searchParams]); // Re-run when searchParams change
 
     if (employeeCode === null) {
+        // You can show a loading indicator until the `employeeCode` is set
         return <div>Loading...</div>;
     }
 
-    return <CardProfilePage employeeCode={employeeCode} />;
+    // Once employeeCode is available, render the CardProfilePage
+    return (
+        <Suspense fallback={<div>Loading Profile...</div>}>
+            <CardProfilePage employeeCode={employeeCode} />
+        </Suspense>
+    );
 }
