722ff60cf92820dd65c1be4897b2c4bc64920f54
Keyur Dobariya <keyur.teqheal@gmail.com>
2025-08-30 14:04:18 +0530

    changes

--- app/(page)/(home)/(panelCommonUtils)/ToolbarUi.js
+++ app/(page)/(home)/(panelCommonUtils)/ToolbarUi.js
@@ -1,3 +1,5 @@
+'use client'
+
 import appColor from "../../../utils/appColor";
 import {Button, Dropdown, Tooltip} from "antd";
 import {MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons";
@@ -8,6 +10,7 @@ import appKeys from "../../../utils/appKeys";
 import SafeAvatar from "../../../components/SafeAvatar";
 import {ChevronDown} from "../../../utils/icons";
 import {profileMenuItems} from "./sideBarMenu";
+import {useEffect, useState} from "react";
 
 export default function ToolbarUi({
                                       isMobile,
@@ -17,6 +20,26 @@ export default function ToolbarUi({
                                       pathname,
                                       menuClick,
                                   }) {
+
+    const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+    useEffect(() => {
+        const handler = (e) => {
+            e.preventDefault();
+            setDeferredPrompt(e);
+        };
+        window.addEventListener("beforeinstallprompt", handler);
+
+        return () => window.removeEventListener("beforeinstallprompt", handler);
+    }, []);
+
+    const handleInstallClick = async () => {
+        if (!deferredPrompt) return;
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        console.log("User choice:", outcome);
+        setDeferredPrompt(null);
+    };
 
     const menuProps = {
         items: profileMenuItems,
@@ -49,6 +72,9 @@ export default function ToolbarUi({
             </div>
             {isMobile && <img src={imagePaths.icon_big_dark} alt="icon" width={150} height={45} />}
             <div className="flex items-center gap-4">
+                <button onClick={handleInstallClick}>
+                    📲 Install App
+                </button>
                 <Dropdown overlayStyle={{ minWidth: 200 }} menu={menuProps} trigger={["click"]}>
                     <div className="flex items-center gap-2 cursor-pointer">
                         <Tooltip title={getLocalData(appKeys.fullName)}>
